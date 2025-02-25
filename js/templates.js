d9a46997fbe94f10b8063ea476d00762d620074a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2025-02-25 22:54:34 +0100

    Added id to the template card.

--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,7 @@
 function cardTemplate({ id, name, img, types }) {
   const typeTags = types.map((type) => `<span class="type-tag">${type}</span>`).join(" ");
   return `
-        <div class="card" style="background-color: var(--${types[0]})" onmouseover="this.style.backgroundColor='var(--${types[0]}-dark)'" onmouseout="this.style.backgroundColor='var(--${types[0]})'">
+        <div id="${id}" class="card" style="background-color: var(--${types[0]})" onmouseover="this.style.backgroundColor='var(--${types[0]}-dark)'" onmouseout="this.style.backgroundColor='var(--${types[0]})'">
           <div>
             <h2 class="card-title">${name}</h2>
             <p class="mt-1">#${id}</p>
